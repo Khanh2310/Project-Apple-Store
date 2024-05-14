8c6db307ed8039bbd1a8596faf84ba75c25af108
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2024-05-14 09:24:50 +0700

    create Carousel atoms

--- src/components/atoms/Carousel/Carousel.tsx
+++ src/components/atoms/Carousel/Carousel.tsx
@@ -1,3 +1,13 @@
-export const Carousel = () => {
-  return <div></div>;
+import { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+  className: string;
+};
+export const Carousel = ({ children, className }: Props) => {
+  return (
+    <>
+      <div className={className}>{children}</div>;
+    </>
+  );
 };
