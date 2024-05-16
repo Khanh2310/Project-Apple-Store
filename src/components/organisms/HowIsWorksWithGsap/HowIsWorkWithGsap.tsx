7bb758d059e68da55f21f163cf9ea58b2ddab50a
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2024-05-16 11:52:31 +0700

    fix bug video

--- src/components/organisms/HowIsWorksWithGsap/HowIsWorkWithGsap.tsx
+++ src/components/organisms/HowIsWorksWithGsap/HowIsWorkWithGsap.tsx
@@ -1,10 +1,12 @@
 import { chipImg, frameImg, frameVideo } from '@/utils';
+import { animateWithGsap } from '@/utils/animation';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { useRef } from 'react';
 
 export const HowIsWorksWithGsap = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
+
   useGSAP(() => {
     gsap.from('#chip', {
       scrollTrigger: {
@@ -16,7 +18,18 @@ export const HowIsWorksWithGsap = () => {
       duration: 2,
       ease: 'power2.inOut',
     });
+
+    animateWithGsap({
+      target: '.g_fadeIn',
+      animationProps: {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: 'power2.inOut',
+      },
+    });
   }, []);
+
   return (
     <section className="common-padding">
       <div className="screen-max-width">
@@ -27,8 +40,9 @@ export const HowIsWorksWithGsap = () => {
         <div className="flex flex-col items-center">
           <h2 className="hiw-title">
             A17 Pro chip.
-            <br />A monster win for gaming.
+            <br /> A monster win for gaming.
           </h2>
+
           <p className="hiw-subtitle">
             It's here. The biggest redesign in the history of Apple GPUs.
           </p>
@@ -47,9 +61,9 @@ export const HowIsWorksWithGsap = () => {
               <video
                 className="pointer-events-none"
                 playsInline
+                preload="none"
                 muted
                 autoPlay
-                preload="none"
                 ref={videoRef}
               >
                 <source src={frameVideo} type="video/mp4" />
@@ -59,27 +73,31 @@ export const HowIsWorksWithGsap = () => {
           <p className="text-gray font-semibold text-center mt-3">
             Honkai: Star Rail
           </p>
+        </div>
 
-          <div className="hiw-text-container">
-            <div className=" flex flex-1 justify-center flex-col">
-              <p className="hiw-text g_fadeIn">
-                A17 Pro is an entirely new class of iPhone chip that delivers
-                our{' '}
-                <span className="text-white">
-                  best graphics performance by far
-                </span>
-                .
-              </p>
-            </div>
+        <div className="hiw-text-container">
+          <div className="flex flex-1 justify-center flex-col">
+            <p className="hiw-text g_fadeIn">
+              A17 Pro is an entirely new class of iPhone chip that delivers our{' '}
+              <span className="text-white">
+                best graphic performance by far
+              </span>
+              .
+            </p>
 
-            <div className="flex-1 flex-center">
-              <p className="feature-text g_text">
-                Titanium has one of the best strength‑to‑weight ratios of any
-                metal, making these our{' '}
-                <span className="text-white">lightest Pro models ever</span>.
-                You’ll notice the difference the moment you pick one up.
-              </p>
-            </div>
+            <p className="hiw-text g_fadeIn">
+              Mobile{' '}
+              <span className="text-white">
+                games will look and feel so immersive
+              </span>
+              , with incredibly detailed environments and characters.
+            </p>
+          </div>
+
+          <div className="flex-1 flex justify-center flex-col g_fadeIn">
+            <p className="hiw-text">New</p>
+            <p className="hiw-bigtext">Pro-class GPU</p>
+            <p className="hiw-text">with 6 cores</p>
           </div>
         </div>
       </div>
